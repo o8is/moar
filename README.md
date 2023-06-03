e1882446c8e64d35b477c72d27433c33b7553909
Jason Stallings <jason@stallin.gs>
2023-06-03 13:42:15 -0500

    feat: Initial commit.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Moar: A Decentralized Dapp Store
+
+## Overview
+
+Moar is a desktop application that enables users to install decentralized application interfaces across various blockchains. 
+
+## Key Features
+
+### DAO (Decentralized Autonomous Organization)
+To prevent spam, low quality dapps and phishing, Moar uses a DAO to approve new dapps or updates, ensuring that the power to shape the platform lies with the users and contributors.
+
+### Local Domain Resolver
+Moar implements a custom DNS resolver specifically designed to handle Moar domains, thereby providing a reliable method for name resolution within the Moar network. Domains are managed by the DAO and do not expire. 
+
+### InterPlanetary File System (IPFS)
+
+IPFS is used for serving the assets of dapps, ensuring data remains distributed and easily accessible. Dapp updates are included in a governance proposal to the DAO with the CID of the new dapp interface. 
+
+Dapp submissions and updates are charged a fee that’s distributed to DAO participants (stakers). This fee helps reduce spam, and generates revenue for maintaining the store. 
+
+When a dapp is installed it will be pinned to the users local IPFS node, ensuring it’s always online and can be installed by other users. 
+
+## Context
+
+In 2015, the Ethereum dapps were accessed via a separate web browser called [Mist](https://github.com/ethereum/mist), which provided support for decentralized hosting through IPFS/Swarm, decentralized DNS via Ethereum Name Service (ENS), and a built-in Ethereum wallet. However, due to challenges in maintaining pace with Chromium's security fixes, Mist was deprecated in 2019.
+
+## Implementation
+To prioritize user security and choice, Moar takes a different approach. Rather than building on top of Chromium, Moar starts a local web server, giving users the freedom to use their preferred browser. This approach ensures the application remains secure, flexible, and adaptable to users' preferences.
+
+## Demo
+
+![](https://publish-01.obsidian.md/access/345d3a51a0287d957b349eed203ad9a4/Images/moar.png)
---
+++ main.js
@@ -0,0 +1,90 @@
+// Modules to control application life and `create` native browser window
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+const http = require('http');
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer({})
+
+async function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 600,
+    height: 600,
+    minWidth: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html');
+}
+
+
+// TODO: Move to DAO.
+const subDomains = {
+  jax: 'QmSnFh2Ad1GnScnUkTLDd8s1UTSwiE1giSEY3j3Ryr1KLv',
+  octalmage: 'QmNUJoP71kUVjyCDpndmJkn7FkHMittn5BfJekguKxwj6s',
+};
+
+const getCID = (hostname) => {
+  const sub = hostname.split('.')[0]
+  if (typeof subDomains[sub] !== 'undefined') {
+    return subDomains[sub];
+  }
+
+  throw new Error('subdomain not found');
+};
+
+// req.headers.host
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(async () => {
+  createWindow()
+
+  try {
+    // Helia is an ESM-only module but Electron currently only supports CJS
+    // at the top level, so we have to use dynamic imports to load it
+    // const node = await fetchDomain("octalma.ge")
+    
+    const server = http.createServer(async function (req, res) {
+      let cid;
+      try {
+        cid = getCID(req.headers.host);
+      } catch (e) {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.write("404 Not Found\n");
+        res.end();
+        return;
+      }
+
+      const fullTarget = `http://127.0.0.1:8080/ipfs/${cid}/`;
+      proxy.web(req, res, { target: fullTarget, followRedirects: false, prependPath: true });
+    });
+
+    console.log("listening on port 8000")
+    server.listen(80);
+
+    // shell.openExternal(node);
+
+  } catch (err) {
+    console.error(err)
+  }
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
