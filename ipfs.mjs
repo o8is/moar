ce696ada9ffb36ade0b69ba902a7ca4a692a90c4
Jason Stallings <octalmage@gmail.com>
2023-06-06 14:19:55 -0500

    fix: Attempt multiple IPFS ports.

--- ipfs.mjs
+++ ipfs.mjs
@@ -1,8 +1,20 @@
 import { create } from 'ipfs-http-client';
+import isPortReachable from 'is-port-reachable';
 
-const client = create({ url: "http://127.0.0.1:5002/api/v0" });
+const getClient = async() => {
+    let port = 5001;
+
+    // IPFS can also run on 5002.
+    if (!await isPortReachable(port, {host: '127.0.0.1'})) {
+        port = 5002;
+    }
+
+    return create({ url: `http://127.0.0.1:${port}/api/v0` });   
+}
+
 
 const getPins = async () => {
+    const client = await getClient();
     const cids = [];
     for await (const { cid } of client.pin.ls({ type: 'recursive' })) {
         cids.push(cid.toString());
@@ -12,6 +24,7 @@ const getPins = async () => {
 };
 
 const addPin = async (cid) => {
+    const client = await getClient();
     return client.pin.add(cid);
 };
 
