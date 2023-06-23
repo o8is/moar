4b170f86156da6823aa897d6565dabe1d96bcc65
Jason Stallings <octalmage@gmail.com>
2023-06-23 17:53:38 -0500

    fix: Correct logic that checks for updates.
    Resolves #17.

--- src/updates.js
+++ src/updates.js
@@ -1,20 +1,16 @@
-const { compareVersions } = require('compare-versions');
+const { compareVersions } = require("compare-versions");
 
 const hasUpdate = async (currentVersion) => {
-  try {
-    const gitopiaResponse = await fetch(
-      "https://api.gitopia.com/gitopia/gitopia/gitopia/Moar/repository/moar-desktop/releases/latest"
-    );
+  const gitopiaResponse = await fetch(
+    "https://api.gitopia.com/gitopia/gitopia/gitopia/Moar/repository/moar-desktop/releases/latest"
+  );
 
-    const latestVersion = (await gitopiaResponse.json()).Release.tagName.slice(1);
-    if (compareVersions(currentVersion, latestVersion) > 0) {
-      throw new Error("no update");
-    }
-
-    return latestVersion;
-  } catch (e) {
-    console.error(e);
+  const latestVersion = (await gitopiaResponse.json()).Release.tagName.slice(1);
+  if (compareVersions(currentVersion, latestVersion) >= 0) {
+    throw new Error("no update");
   }
+
+  return latestVersion;
 };
 
 module.exports = { hasUpdate };
