b8099476171646fb5650e0fc81b1f3a72dbf84fb
Jason Stallings <jason@stallin.gs>
2023-06-24 12:26:40 -0500

    feat: Enable keyboard navigation.
    Use tab to select a dapp, then hit enter to launch.

--- src/App.svelte
+++ src/App.svelte
@@ -38,66 +38,65 @@
             </div>
             <div class="container mx-auto px-4 pb-8">
                 <div class="flex flex-wrap -mx-1 lg:-mx-4">
-                    {#each dapps as dapp, index}
+                    {#each dapps as dapp}
                         <!-- Column -->
                         <div
                             class="w-full md:w-1/2 my-4 px-4 lg:w-1/3 not-draggable"
                         >
                             <!-- Article -->
-                            <article
-                                class="bg-gray-800 overflow-hidden rounded-lg shadow-lg"
-                            >
-                                <a target="_blank" href={`http://${dapp.domain}`}>
+                            <a target="_blank" href={`http://${dapp.domain}`}>
+                                <article
+                                    class="bg-gray-800 overflow-hidden rounded-lg shadow-lg"
+                                >
                                     <img
                                         alt="Screenshot"
                                         class="block h-auto w-full"
                                         src="http://127.0.0.1:8080/ipfs/{dapp.preview}/"
                                     />
-                                </a>
+                                    <header
+                                        class="flex items-center justify-between leading-tight p-2 md:p-4"
+                                    >
+                                        <h1 class="text-lg">
+                                            <span
+                                                class="text-white no-underline hover:underline text-black"
+                                            >
+                                                {dapp.name}
+                                            </span>
+                                        </h1>
+                                        <p class="text-gray-400 text-sm">
+                                            Updated <Time
+                                                timestamp={dapp.versions[0].date}
+                                                live
+                                                relative
+                                            />
+                                        </p>
+                                    </header>
 
-                                <header
-                                    class="flex items-center justify-between leading-tight p-2 md:p-4"
-                                >
-                                    <h1 class="text-lg">
-                                        <a
-                                            target="_blank"
-                                            class="text-white no-underline hover:underline text-black"
-                                            href={`http://${dapp.domain}`}
-                                        >
-                                            {dapp.name}
-                                        </a>
-                                    </h1>
-                                    <p class="text-gray-400 text-sm">
-                                        Updated <Time
-                                            timestamp={dapp.versions[0].date}
-                                            live
-                                            relative
-                                        />
-                                    </p>
-                                </header>
-
-                                <footer
-                                    class="flex items-center justify-between leading-none p-2 md:p-4"
-                                >
-                                    <span class="text-gray-400">
-                                        {dapp.tags[0]}
-                                    </span>
-                                    <span class="text-sm">
-                                        <select
-                                            class="bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
-                                            on:change={onVersionSelect}
-                                        >
-                                            {#each dapp.versions as version}
-                                                <option
-                                                    value={`${dapp.domain}|${version.cid}`}
-                                                >
-                                                    v{version.id}
-                                                </option>
-                                            {/each}
-                                        </select>
-                                    </span>
-                                </footer>
-                            </article>
+                                    <footer
+                                        class="flex items-center justify-between leading-none p-2 md:p-4"
+                                    >
+                                        <span class="text-gray-400">
+                                            {dapp.tags[0]}
+                                        </span>
+                                        <span class="text-sm">
+                                            <select
+                                                on:click|preventDefault
+                                                tabindex="-1"
+                                                class="bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
+                                                on:change={onVersionSelect}
+                                            >
+                                                {#each dapp.versions as version}
+                                                    <option
+                                                        value={`${dapp.domain}|${version.cid}`}
+                                                    >
+                                                        v{version.id}
+                                                    </option>
+                                                {/each}
+                                            </select>
+                                        </span>
+                                    </footer>
+                                </article>
+                            </a>
                             <!-- END Article -->
                         </div>
                         <!-- END Column -->
